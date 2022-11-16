e2757efd59fe2f0438d8453c68b0cbad99e297a5
Max Callaghan <max.w.callaghan@gmail.com>
2022-11-16 11:46:43 +0100

    fix publications

--- bibliography/bib_template.md
+++ bibliography/bib_template.md
@@ -1,9 +1,9 @@
 ---
 title: "{{ e.title }}"
 collection: publications
-permalink: /publication/{{i}}
+permalink: /publications/{{i}}
 date: {{e.year}}-01-01
-venue: '{{ e.journal }}'
-paperurl: 'http://academicpages.github.io/files/paper1.pdf'
-citation: '{{e.author}}. ({{ e.year }}). &quot;{{ e.title }}.&quot; <i>{{ e.journal }}</i>. {{e.volume}}({{e.number}}).'
+venue: "{{ e.journal }}"
+citation: "{{e.author}}. ({{ e.year }}). &quot;{{ e.title }}.&quot; <i>{{ e.journal }}</i>. {{e.volume}}({{e.number}})."
+doi: "{{e.doi}}"
 ---
--- prepare_bib.py
+++ prepare_bib.py
@@ -4,6 +4,10 @@ import os
 import glob
 import jinja2
 import numpy as np
+import re
+from datetime import datetime as dt
+from pylatexenc.latex2text import LatexNodes2Text
+
 
 files = glob.glob('_publications/*')
 for f in files:
@@ -21,26 +25,31 @@ templateEnv = jinja2.Environment( loader=templateLoader )
 template = templateEnv.get_template( "bib_template.md" )
 
 parser = BibTexParser(common_strings=True)
-with open('bibliography/publications.bib') as file:
+with open('bibliography/callaghan-publications.bib') as file:
     bib_db = bibtexparser.load(file, parser)
 
 #os.remove("_cv/nocite.tex")
 
+bib_db.entries = [x for x in bib_db.entries if re.match("\w",x["title"])]
 
-dates = np.array([int(e["year"]) for e in bib_db.entries])
+dates = np.array([dt.strptime(f'{e["year"]}-{e["month"]}',"%Y-%B") for e in bib_db.entries])
 
 print(dates)
 
-for i in np.argsort(-dates):
+n = 0
+for i in np.argsort(dates)[::-1]:
+    n+=1
     e = bib_db.entries[i]
+    print(e)
     e["title"] = e["title"].replace("{","").replace("}","")
-    fname = f"_publications/{i:03d}.md"
+    fname = f"_publications/{n:03d}.md"
     authors = e["author"].split(" and ")
     authors = ["<b>"+x+"</b>" if "Callaghan" in x else x for x in authors]
     e["author"] = ", ".join(authors)
     e["journal"] = e["journal"].replace("\\","")
+    e["author"] = LatexNodes2Text().latex_to_text(e["author"])
     with open("_cv/nocite.tex", "a") as f:
         f.write(r"\nocite{" + e["ID"] + "}")
         f.write("\n")
     with open(fname, "w") as f:
-        f.write(template.render({"e":e, "i": i}))
+        f.write(template.render({"e":e, "i": n}))
